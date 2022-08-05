b200d0f7db25b233875db6a3684cd61e2c4fefcf
Vladimir <devvladimirobertas@gmail.com>
2022-08-06 00:34:05 +0300

    Maps refactoring

--- app/src/main/java/com/example/mibus/MainActivity.kt
+++ app/src/main/java/com/example/mibus/MainActivity.kt
@@ -6,21 +6,43 @@ import android.os.Bundle
 
 import android.view.View
 import android.widget.Button
-import com.example.mibus.databinding.ActivityMapsBinding
+import com.example.mibus.databinding.ActivityMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
 class MainActivity : AppCompatActivity(),View.OnClickListener {
 
- private lateinit var binding: ActivityMapsBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+            // Ensure all places are visible in the map
+            googleMap.setOnMapLoadedCallback {
+                val sydney = LatLng(-34.0, 151.0)
+                googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+            }
+
+            //addMarkers(googleMap)
 
 
-       val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener(this)
+
+            // Set custom info window adapter
+            // googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+        }
+
+
+
+     //  val button = findViewById<Button>(R.id.button)
+     ///   button.setOnClickListener(this)
 
 
 
@@ -28,8 +50,8 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
 
 
     override fun onClick(v: View?) {
-        intent = Intent(this,MapsActivity::class.java)
-        startActivity(intent)
+     //   intent = Intent(this,MapsActivity::class.java)
+     //   startActivity(intent)
     }
 
 }
