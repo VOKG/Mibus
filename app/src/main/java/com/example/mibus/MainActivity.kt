e948a82c9898c66bc538001d18972f5e385f0962
Vladimir <devvladimirobertas@gmail.com>
2022-09-14 19:26:40 +0300

    new navigation

--- app/src/main/java/com/example/mibus/MainActivity.kt
+++ app/src/main/java/com/example/mibus/MainActivity.kt
@@ -1,67 +1,36 @@
 package com.example.mibus
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-import android.view.View
-import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import com.example.mibus.databinding.ActivityMainBinding
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MainActivity : AppCompatActivity(),View.OnClickListener {
 
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         setContentView(binding.root)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "MIBUS"
-
+       // supportActionBar?.setDisplayHomeAsUpEnabled(true)
+       // supportActionBar?.title = "MIBUS"
 
 
        // setSupportActionBar(binding.toolbar)
 
-
-
-        val mapFragment =
-            supportFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
-        mapFragment?.getMapAsync { googleMap ->
-            // Ensure all places are visible in the map
-            googleMap.setOnMapLoadedCallback {
-                val sydney = LatLng(-34.0, 151.0)
-                googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-                googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-            }
-
-            //addMarkers(googleMap)
-
-
-
-            // Set custom info window adapter
-            // googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
-        }
-
-
-
      //  val button = findViewById<Button>(R.id.button)
      ///   button.setOnClickListener(this)
 
-
-
     }
 
 
-    override fun onClick(v: View?) {
-     //   intent = Intent(this,MapsActivity::class.java)
-     //   startActivity(intent)
-    }
+
+
+
+
+
 
 }
---
+++ app/src/main/java/com/example/mibus/ViewModel/ViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.mibus.ViewModel
+
+import com.google.android.gms.maps.GoogleMap
+
+interface ViewModel {
+
+    fun setPointClick(map:GoogleMap)
+
+   fun setMapLongClick(map: GoogleMap)
+
+}
